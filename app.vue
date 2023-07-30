cc9457847f4788b12e6199318ff0be01c847c121
Pesar <pesar369@gmail.com>
2023-07-30 20:17:15 +0330

    init

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ `ByteCast is building${dots}` }}</div>
+</template>
+<script lang="ts" setup>
+const dots = ref<string>("")
+
+setInterval(() => {
+  if (dots.value.length < 6) dots.value += "."
+  else dots.value = ""
+}, 500)
+</script>
+<style>
+div {
+  font-family: monospace;
+  font-size: 24px;
+}
+</style>
